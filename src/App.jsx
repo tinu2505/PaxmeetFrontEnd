0b1dd5aa977f6456e8aac1aa2835ffd7dbda7065
Tushar Tandon <tinu2505@gmail.com>
2025-12-18 16:34:32 +0530

    error resolve

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Footer from './components/Foooter';
 import Home from './pages/Home';
 import Events from './pages/Events';
